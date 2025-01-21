e93a5bbf58ccbdfebf699d08594be6414c22d071
liamstonestreet <141878023+liamstonestreet@users.noreply.github.com>
2025-01-21 13:57:03 -0600

    Update emotion.py
    Fixed the name of 'chords_to_concepts', changed it to 'extract_chords', which the function actually does.

--- emotion.py
+++ emotion.py
@@ -1,4 +1,4 @@
-"""
+x"""
 This file contains various emotional analysis methods, that accept an audio file as input
 and return either chords (groupings of harmonics) or "concepts" (mappings of interval changes to text descriptions).
 """
@@ -47,7 +47,7 @@ def map_interval_changes_to_concepts(chords: list[Chord]) -> list[str]:
     
     return concepts
 
-def chord_to_concepts(y: np.ndarray, sr: int, num_windows: int) -> list[Chord]:
+def extract_chords(y: np.ndarray, sr: int, num_windows: int) -> list[Chord]:
     """
     Does chord detection on a given input audio array and returns a list of chords
     @param y: the audio array
